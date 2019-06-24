aca520e77e0a5003cce8a0407c1407c3dbb3289b
AnatoliyZ <zolotukhinwebpr@mail.ru>
2019-06-25 00:52:31 +0700

    test fetch

--- src/api.js
+++ src/api.js
@@ -1,1 +1,14 @@
 const ENDPOINT = 'https://simple-api.sandbox.movavi.com/api/v1/';
+
+const testFetch = fetch('https://simple-api.sandbox.movavi.com/api/v1/', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    jsonrpc: '2.0',
+    id: 'test',
+    method: 'readCategory',
+    params: {
+      conditions: ['id', '=', '114']
+    }
+  })
+}).then(resp => resp.json()).then(data => console.log(data.result));
