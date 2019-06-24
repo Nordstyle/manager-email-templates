94a4a1cbee78aa81e3498a5266cf237de79b2b0f
AnatoliyZ <zolotukhinwebpr@mail.ru>
2019-06-24 23:14:37 +0700

    page stubs

---
+++ src/pages/Categories/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div> Categroies </div>
+  );
+};
+
+export default Categories;
---
+++ src/pages/Index/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <h1 style={{ textAlign: 'center' }}> Hello Movavi </h1>
+  )
+};
+
+export default Index;
