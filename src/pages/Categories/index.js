8ab6b2af4bee87313ad3838adb7c059bcd7a3a84
Zolotukhin <zolotukhinwebpr@mail.ru>
2019-06-25 11:51:02 +0700

    category

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import {Typography} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
-const Categories = () => {
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1
+  }
+}));
+
+const Categories = (props) => {
+  const classes = useStyles();
   return (
-    <div> Categroies </div>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={2}>
+          <Typography variant={'h6'}>
+            Actions
+          </Typography>
+          <ButtonGroup fullWidth color={'primary'}>
+            <Button>
+              Add item
+            </Button>
+          </ButtonGroup>
+        </Grid>
+        <Grid item xs={10}>
+          <Typography variant={'h6'}>
+            List of Categories
+          </Typography>
+          <div> table </div>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
