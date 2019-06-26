f8943a0409493a0671747c9705fc13c7f72d41a8
AnatoliyZ <zolotukhinwebpr@mail.ru>
2019-06-26 21:39:00 +0700

    validators inputs form

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
@@ -53,12 +53,14 @@ const FormDialog = props => {
     updateMethod
   } = props;
   const classes = useStyles();
+  const [validate, setValidate] = useState({ length: true, update: true });
   const actionTitle = effect === "add" ? "Add" : "Update";
   const isRowAction = effect === "update";
   return (
     <Dialog
       open={open}
       onClose={() => modalHandler({ type: "close" })}
+      onEnter={() => isRowAction ? setValidate({ length: true, update: false }) : setValidate({ length: true, update: true })}
       aria-labelledby="form-dialog-title"
       transitionDuration={0}
     >
@@ -70,7 +72,8 @@ const FormDialog = props => {
             { effect, payload },
             addMethod,
             deleteMethod,
-            updateMethod
+            updateMethod,
+            validate
           )
         }
       >
@@ -80,18 +83,26 @@ const FormDialog = props => {
             autoFocus
             margin="dense"
             id="title"
-            label="Category title"
+            label={ !validate.length ? 'Maximum string length 250 characters' : 'Title'}
             defaultValue={isRowAction ? payload.title : ""}
             type="text"
             required
             className={classes.input}
+            onChange={(e) => {
+              if (e.target.value.length >= 250) {
+                setValidate({ ...validate, length: false })
+              } else if (isRowAction && e.target.value === payload.title) {
+                setValidate({ ...validate, update: false })
+              } else {
+                setValidate({ length: true, update: true })
+              }
+            }}
             fullWidth
           />
           <TextField
-            autoFocus
             margin="dense"
             id="parent"
-            label="Category parent id"
+            label="Parent id"
             defaultValue={isRowAction ? payload.id : ""}
             type="number"
             className={classes.input}
@@ -107,7 +118,9 @@ const FormDialog = props => {
             >
               Cancel
             </Button>
-            <Button type="submit">{actionTitle}</Button>
+            <Button
+              disabled={!validate.length || !validate.update }
+              type="submit">{actionTitle}</Button>
           </ButtonGroup>
         </DialogActions>
       </form>
